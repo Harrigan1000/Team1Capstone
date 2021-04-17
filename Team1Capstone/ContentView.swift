05458acffde6f9605d1b5a43fc35edba61821a9f
Bernard Harrigan <bharrigan2011@gmail.com>
2021-04-17 10:18:17 -0500

    Initial Commit

---
+++ Team1Capstone/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Team1Capstone
+//
+//  Created by Bernard Harrigan on 4/17/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Team1Capstone/Team1CapstoneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Team1CapstoneApp.swift
+//  Team1Capstone
+//
+//  Created by Bernard Harrigan on 4/17/21.
+//
+
+import SwiftUI
+
+@main
+struct Team1CapstoneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
