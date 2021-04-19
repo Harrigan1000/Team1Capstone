28b68a68a79ca4170fc7f450b32a2b6128dd56ee
Bernard Harrigan <bharrigan2011@gmail.com>
2021-04-19 12:40:36 -0500

    3rd Commit.  I have added all the pages and links to each page.  Still working on the links to all screens

--- Team1Capstone/HealthHabitsView.swift
+++ Team1Capstone/HealthHabitsView.swift
@@ -8,11 +8,43 @@
 import SwiftUI
 
 struct HealthHabitsView: View {
+   //Mark Properties:
+    
+    @State var mainBullets = ["Eating Habits", "Nutritional Needs", "Meal Preparation", "Track Nutrition"]
+    
+    // Mark Body:
     var body: some View {
-        Text("Health Habits")
-    }
-}
+        NavigationView {
+            VStack {
+                TextField("Health Habits", text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant("")/*@END_MENU_TOKEN@*/)
+                    .padding([.leading, .bottom, .trailing])
+                    .font(/*@START_MENU_TOKEN@*/.largeTitle/*@END_MENU_TOKEN@*/)
+                    .foregroundColor(/*@START_MENU_TOKEN@*/.red/*@END_MENU_TOKEN@*/)
+                    .multilineTextAlignment(/*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                
+                TextField("Nutritional Needs Suggestions", text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant("")/*@END_MENU_TOKEN@*/)
+                    .multilineTextAlignment(/*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                    .font(/*@START_MENU_TOKEN@*/.title2/*@END_MENU_TOKEN@*/)
+                
+                
+                List(mainBullets, id: \.self) {
+                    bullets in
+                    NavigationLink(
+                        destination: MindfulnessView(),
+                        label: {
+                            Text(bullets)
+                        })
+                    .padding()
+                }
+            }//VSTACK END POINT
+            .onAppear {
+                mainBullets.sort()
+            }
+        }// NAVIGATIONVIEW END POINT
+    }// BODY END POINT
+}// STRUCT END POINT
 
+// Mark Preview:
 struct HealthHabitsView_Previews: PreviewProvider {
     static var previews: some View {
         HealthHabitsView()
